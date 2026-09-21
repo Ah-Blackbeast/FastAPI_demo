7ec38f2f036aa3bed20acbc096d791ba93acf1d5
Alok <your_github_email@example.com>
2026-09-21 15:29:13 +0530

    queryparam

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
+app = FastAPI()
 
-app= FastAPI()
+@app.get("/user")
 
-@app.get("/")
-def home():
-    return{"message": "hello death"}
+def get_users(limit:int=5, quantity: int=None ,price: int=None, name: str = None):
+    return{
+        "name": name,
+        "price":price,
+        "quantity": quantity,
+        "max no of item get": limit
+    }
 
-@app.get("/about")
-def about():
-    return{"about":"i am death that comes for every single one"}
-
-@app.get("/price")
-def price():
-    return {"price": ["int", "float", "roaming"]}
-
-@app.get("/users/{user_ID}")
-def get_user(user_ID:int):
-    return{"user id": user_ID}
+# def get_users(limit:int=5):
+#     return{
+#         "no of quantity get(max value)":limit
+#     }
